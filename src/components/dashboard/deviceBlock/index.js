8c50d46e6f691b9443f0461803f055a239b038c9
Dmitry <d-piv@mail.ru>
2020-02-20 16:19:10 +0300

    Add images to device block

---
+++ src/components/dashboard/deviceBlock/index.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+import "./deviceBlock.css"
+import lamp_png from "../../../images/lamp.png"
+
+export default class DeviceBlock extends Component {
+    device = this.props.devices;
+
+    render() {
+        return (
+            <div className="device-block">
+                <img className="" src={lamp_png} alt=""/>
+                <h4>{this.device.name}</h4>
+                <p>{this.device.body}</p>
+                <p><small>{this.device.type}</small></p>
+                <button type={"sumbit"} className="btn btn-red">ON</button>
+            </div>
+        );
+    }
+}
--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,10 +1,34 @@
 import React, {Component} from "react";
+import DeviceBlock from "./deviceBlock";
+
 
 export default class DashboardPage extends Component {
+    // folder_img = "../../../images";
+    devices = [
+        {
+            name:"Розетка Wi-Fi",
+            body:"sdasdadsasd",
+            type:"bfg-2000",
+            url:"lamp.png"
+        },
+        {
+            name:"Одноканальный выключатель",
+            body:"asgsgjsfgsgsb",
+            type:"dh-13",
+            url:"lamp.png"
+        },
+        {
+            name:"Датчик протечки",
+            body:"sdasd35363dzfbzfbzfdbzdfbd",
+            type:"zigbee-2020",
+            url:"lamp.png"
+        }];
+
     render() {
         return (
-            <div>
+            <div className="dashboard-main">
                 Dashboard!!!
+                {this.devices.map((item, i) => <DeviceBlock devices={item}/>)}
             </div>
         );
     }
